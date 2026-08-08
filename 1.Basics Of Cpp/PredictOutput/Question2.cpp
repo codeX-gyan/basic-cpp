24b0d37ff281912531a94b08e57ce5ce768df994
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-08 17:31:56 +0100

    Questions based on infinite loops

---
+++ 1.Basics Of Cpp/PredictOutput/Question2.cpp
@@ -0,0 +1,22 @@
+//-----INFINITE LOOP------
+
+#include<iostream>
+using namespace std;
+int main() {
+    int i;
+    while( i= 10 ) {
+    cout<<i<<endl;
+    i=i+1;
+    }
+}
+
+
+// ------INFINITE LOOP--------
+// SCAM- WE DO NOT COMPARE CHARACTER HERE WE COMPARE ASCII VALUE
+#include<iostream>
+using namespace std;
+int main() {
+    while('a'<'b')
+    cout<<"you are noob"<<endl;   
+}
+ 
