2aafd751c4f4fd468164a9adedade3ec41ea453a
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-08 16:05:44 +0100

    Update basics.cpp

--- 1.Basics Of Cpp/basics.cpp
+++ 1.Basics Of Cpp/basics.cpp
@@ -1,9 +1,9 @@
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int main(){
-//     int a=5 , b=6, sum;
-//     sum = a+b;
-//     cout<<sum;
-// }
+int main(){
+    int a=5 , b=6, sum;
+    sum = a+b;
+    cout<<sum;
+}
 
