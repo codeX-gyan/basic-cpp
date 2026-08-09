72455900ba0b094923e5dd081d36f51dd11464db
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-09 12:49:03 +0100

    Update PnC.cpp

--- 1.Basics Of Cpp/Functions/PnC.cpp
+++ 1.Basics Of Cpp/Functions/PnC.cpp
@@ -1,5 +1,43 @@
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cout << "Enter n : ";
+//     cin >> n;
+//     int r;
+//     cout << "Enter r : ";
+//     cin >> r;
+
+//     int a=1;
+//     for (int i= 1; i <= n; i++) {
+//         a *= i;
+//     }
+//     int b=1;
+//     for (int i= 1; i <= r; i++) {
+//         b *= i;
+//     }
+
+//     int c=1;
+//     for (int i= 1; i <= n-r ; i++) {
+//         c *= i;
+//     }
+//     cout << a/(b*c);
+// }
+
+
+
+// --------COMBINATION USING FUNCTIONS----------
+
 #include<iostream>
 using namespace std;
+int fact( int x ) {
+    int f = 1;
+    for( int i =1 ; i<=x ; i++){
+        f*=i;
+    }
+    return f ;
+}
 
 int main() {
     int n;
@@ -9,18 +47,9 @@ int main() {
     cout << "Enter r : ";
     cin >> r;
 
-    int a=1;
-    for (int i= 1; i <= n; i++) {
-        a *= i;
-    }
-    int b=1;
-    for (int i= 1; i <= r; i++) {
-        b *= i;
-    }
+    int a=fact(n);
+    int b=fact(r);
+    int c=fact(n-r);
 
-    int c=1;
-    for (int i= 1; i <= n-r ; i++) {
-        c *= i;
-    }
     cout << a/(b*c);
 }
