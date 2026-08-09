fff0c35fa5391d2523d6f7d29e36078d61fb42ca
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-09 11:02:06 +0100

    Create basicfunction.cpp

---
+++ 1.Basics Of Cpp/Functions/basicfunction.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+void greet() {
+    cout<<"hey gyan"<<endl;
+    cout<<"how are you buddy"<<endl;
+}
+void send_off() {
+    cout<<"nice to meet you"<<endl;
+    cout<<"bye buddy"<<endl;
+}
+
+int main() {
+    greet();
+    cout<<"all the chats are successfully have a great day"<<endl;
+    send_off();
+}
+
+
+
+// #include<iostream>
+// using namespace std;
+// void sum() {
+//     int a,b,sum;
+//     sum = a+b;
+//     return;
+// }
+
+// // void product() {
+// //     int a,b,product;
+// //     product = a*b;
+// // }
+
+// int mian() {
+//     int a =4,b=5;
+//     // cout<<"sum of numbers is : " <<endl;
+//     sum();
+//     // product();
+// }
