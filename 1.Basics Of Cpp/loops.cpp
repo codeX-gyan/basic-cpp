157ccf51fc85e97c63acd53b15fb725805eb8fe2
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-09 12:23:04 +0100

    Update loops.cpp

--- 1.Basics Of Cpp/loops.cpp
+++ 1.Basics Of Cpp/loops.cpp
@@ -201,4 +201,20 @@
 // }
 
 
-//    -----WHILE LOOP-------
+//    -----FACTORIALS------
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+
+    int product = 1;
+
+    for (int x = 1; x <= n; x++) {
+        product *= x;
+    }
+
+    cout << product;
+}
