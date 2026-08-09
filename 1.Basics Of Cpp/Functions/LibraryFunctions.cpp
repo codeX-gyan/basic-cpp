e4ee5ec67ea9ba0245d2682e2339fb6eb0affa1a
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-09 11:57:43 +0100

    library functions

--- 1.Basics Of Cpp/Functions/LibraryFunctions.cpp
+++ 1.Basics Of Cpp/Functions/LibraryFunctions.cpp
@@ -7,3 +7,28 @@ int main() {
     cout<<cbrt(729)<<endl;
     cout<<9*3.24*14;
 }
+
+
+//--------MIN MAX----------
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+    // cout<<min(4,2)<<endl;
+    // cout<<max(6,4)<<endl;
+
+    cout<<min((4,2),6)<<endl;
+    cout<<max((6,4),8)<<endl;
+}
+
+
+//--------POW--------
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+    cout<<pow(2,6)<<endl;
+    cout<<pow(3,6)<<endl;
+}
