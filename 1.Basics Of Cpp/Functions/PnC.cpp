5317cfe322612f7caffa4c8c980b25ef47dc8686
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-10 23:15:52 +0100

    Update PnC.cpp

--- 1.Basics Of Cpp/Functions/PnC.cpp
+++ 1.Basics Of Cpp/Functions/PnC.cpp
@@ -29,35 +29,6 @@
 
 // --------COMBINATION USING FUNCTIONS----------
 
-#include<iostream>
-using namespace std;
-int fact( int x ) {
-    int f = 1;
-    for( int i =1 ; i<=x ; i++){
-        f*=i;
-    }
-    return f ;
-}
-
-int main() {
-    int n;
-    cout << "Enter n : ";
-    cin >> n;
-    int r;
-    cout << "Enter r : ";
-    cin >> r;
-
-    int a=fact(n);
-    int b=fact(r);
-    int c=fact(n-r);
-
-    cout << a/(b*c);
-}
-
-
-
-
-// --------PERMUTATION---------
 // #include<iostream>
 // using namespace std;
 // int fact( int x ) {
@@ -77,7 +48,36 @@ int main() {
 //     cin >> r;
 
 //     int a=fact(n);
-//     int b=fact(n-r);
+//     int b=fact(r);
+//     int c=fact(n-r);
 
-//     cout << a/b;
+//     cout << a/(b*c);
 // }
+
+
+
+
+// --------PERMUTATION---------
+#include<iostream>
+using namespace std;
+int fact( int x ) {
+    int f = 1;
+    for( int i =1 ; i<=x ; i++){
+        f*=i;
+    }
+    return f ;
+}
+
+int main() {
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    int r;
+    cout << "Enter r : ";
+    cin >> r;
+
+    int a=fact(n);
+    int b=fact(n-r);
+
+    cout << a/b;
+ }
