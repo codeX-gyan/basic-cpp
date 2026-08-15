d3a96f6bdb4b2d78a0e84cec7a83e7aef94ab3cb
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-15 15:58:16 +0100

    Create powerfuncton.cpp

---
+++ Recursion/powerfuncton.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int pow(int a , int b) {
+    if(b==0) return 1;
+    return a*pow(a,b-1);
+    
+}
+
+int main() {
+    int a,b;
+    cout<<"enter a : ";
+    cin>>a;
+    cout<<"enter b : ";
+    cin>>b;
+
+    cout<<pow(a,b);
+}
