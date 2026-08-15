76cae619d07f159e8c5849c9b769d734f8259574
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-15 15:58:42 +0100

    Update factorial.cpp

--- Recursion/factorial.cpp
+++ Recursion/factorial.cpp
@@ -1,15 +1,16 @@
-#include<iostream>
-using namespace std;
-int fact(int n) {
-    if(n==1) return 1;
-    return n*fact(n-1);
+// #include<iostream>
+// using namespace std;
+// int fact(int n) {
+//     if(n==1) return 1;
+//     return n*fact(n-1);
     
-}
+// }
 
-int main() {
-    int n;
-    cout<<"enter a number : ";
-    cin>>n;
+// int main() {
+//     int n;
+//     cout<<"enter a number : ";
+//     cin>>n;
 
-    cout<<fact(n);
-}
+//     cout<<fact(n);
+// }
+
