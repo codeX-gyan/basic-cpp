396ac4f494987f836649ef6b7328f57b53c9af34
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-18 09:56:10 +0100

    Update Swap2nums.cpp

--- 1.Basics Of Cpp/Functions/Swap2nums.cpp
+++ 1.Basics Of Cpp/Functions/Swap2nums.cpp
@@ -1,31 +1,32 @@
 // -----SWAP USING THIRD VARIABLE--------
 
-// #include<iostream>
-// using namespace std;
-// int main() {
-//     int temp;
-//     int a=5;
-//     int b=7;
-//     temp = a;
-//     a = b;
-//     b = temp;
-//     cout<<a<<endl;
-//     cout<<b;
-// }
-
-
-
-//----SWAP EITHOUT THIRD VARIABLE----
-
 #include<iostream>
 using namespace std;
 int main() {
-    // int temp;
-    int a=675;
-    int b=827;
-    a = a+b;
-    b = a-b;
-    a = a-b;
+    int temp;
+    int a=5;
+    int b=7;
+    temp = a;
+    a = b;
+    b = temp;
     cout<<a<<endl;
     cout<<b;
-} 
+}
+
+
+
+//----SWAP WITHOUT THIRD VARIABLE----
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     // int temp;
+//     int a=675;
+//     int b=827;
+//     a = a+b;
+//     b = a-b;
+//     a = a-b;
+//     cout<<"after swapping : ";
+//     cout<<a<<endl;
+//     cout<<b;
+// } 
