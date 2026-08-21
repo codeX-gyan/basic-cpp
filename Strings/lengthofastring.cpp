5a132e9ba2bc1c10495ce3f72f0ec1e5bb01b32c
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-21 10:35:37 +0100

    Create lengthofastring.cpp

---
+++ Strings/lengthofastring.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    string s = "bro fucked up ho gya hu";
+    // cout<<s[7];
+    // cout<<endl;
+    // s[6] = 'S';
+    // cout<<s;
+    cout<<s.length();
+}
