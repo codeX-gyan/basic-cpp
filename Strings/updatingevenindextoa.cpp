ebe4a11e3f76c3e3d06d91e4894eac07cb25df32
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-21 10:55:08 +0100

    Create updatingevenindextoa.cpp

---
+++ Strings/updatingevenindextoa.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    string s = "chal be bitch";
+    int n = s.length();
+    for( int i = 0; i<n ; i++){
+        if(i%2 == 0){
+           s[i] =  'a';
+        }
+    }
+    cout<<s<<" ";
+}
