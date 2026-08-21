fcd3b1cba9495748f5d6c56383f8b9338abdce2f
codeX-gyan <gyanprakashpatna2007@gmail.com>
2026-08-21 11:15:17 +0100

    Create plusoperators.cpp

---
+++ Strings/plusoperators.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    string s = "gyan";
+    cout<<s<<" "<<s.length();
+    cout<<endl;
+    s = s + " prakash";
+    cout<<s<<" "<<s.length();
+}
